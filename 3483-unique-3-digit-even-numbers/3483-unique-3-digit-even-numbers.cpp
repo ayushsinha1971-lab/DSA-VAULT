18945a462428b6c3e2024ec1c999d42d9a11eda0
Ayush Sinha <ayushsinha1971@gmail.com>
2026-09-11 19:54:22 +0530

    Time: 3 ms (84.06%), Space: 33.9 MB (14.79%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -3,23 +3,32 @@ public:
     int totalNumbers(vector<int>& digits) {
         int n=digits.size();
         int ans=0;
+        vector <int > count(10);
+        for(auto it: digits){
+            count[it]++;
+        }
         vector <int> vis(1000,0);
-        for(int i=0;i<n;i++){
-            if(digits[i]==0){
-                continue;
-            }
-            for(int j=0;j<n;j++){
-                if(i==j)continue;
-                for(int k=0;k<n;k++){
-                    if(i==k||j==k||digits[k]%2!=0)continue;
-                    int x=100*digits[i]+10*digits[j]+digits[k];
+        for(int i=1;i<10;i++){
+            if(count[i]==0)continue;
+            count[i]--;
+            for(int j=0;j<10;j++){
+                if(count[j]==0){
+                    continue;
+                }
+                count[j]--;
+                for(int k=0;k<10;k+=2){
+                    if(count[k]==0)continue;
+                    
+                    int x=100*i+10*j+k;
                     if(vis[x]==0){
                         vis[x]=1;
                         ans++;
                     }
+
                 }
+                count[j]++;
             }
-            
+            count[i]++;
         }
         return ans;
     }
